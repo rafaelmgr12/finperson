ee13daecbd54bc23a6b910cb52d027b9e2d339d7
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-08-07 01:21:01 -0300

    Start project

---
+++ src/main/java/com/rafaelmgr12/finperson/FinpersonApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.finperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinpersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinpersonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafaelmgr12/finperson/FinpersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.finperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinpersonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
