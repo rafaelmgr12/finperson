2f4bf900a85a73baf1b463bd8ae7d264deff0ea4
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-08-08 11:17:49 -0300

    Add Entity Expense

---
+++ src/main/java/com/rafaelmgr12/finperson/entity/Expense.java
@@ -0,0 +1,48 @@
+package com.rafaelmgr12.finperson.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "expense")
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String description;
+    private Double value;
+    private LocalDate date;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
